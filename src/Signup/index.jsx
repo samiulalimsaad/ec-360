0d458300c312abef0b3aa507e34af1f618970b9d
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-23 12:46:50 +0600

    formik added in signup page

--- src/Signup/index.jsx
+++ src/Signup/index.jsx
@@ -1,3 +1,4 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React, { useEffect, useState } from "react";
 import {
     useAuthState,
@@ -7,6 +8,7 @@ import {
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { auth } from "../firebase.init";
 import useTitle from "../utilities/useTitle";
+import { signUpValidationSchema } from "../validator";
 
 const Signup = () => {
     useTitle("SignUp");
@@ -48,74 +50,100 @@ const Signup = () => {
     };
     return (
         <div className="flex items-center justify-center p-4 sm:container sm:p-20">
-            <form className="sm:w-1/3" onSubmit={createUser}>
-                <div className="p-4 px-8 pt-6 pb-8 mb-4 bg-white border rounded-md shadow-md border-slate-500">
-                    {(error || message) && (
-                        <p className="p-4 mb-4 bg-red-200 rounded-md">
-                            {error?.message || message}
-                        </p>
-                    )}
-                    <div className="mb-4 form-control">
-                        <label
-                            className="block mb-2 text-sm font-bold text-slate-700"
-                            htmlFor="name"
-                        >
-                            Name
-                        </label>
-                        <input
-                            className="w-full px-3 py-2 leading-tight border rounded shadow appearance-none text-slate-700 focus:outline-none focus:shadow-outline"
-                            id="name"
-                            type="text"
-                            name="name"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4 form-control">
-                        <label
-                            className="block mb-2 text-sm font-bold text-slate-700"
-                            htmlFor="email"
-                        >
-                            Email
-                        </label>
-                        <input
-                            className="w-full px-3 py-2 leading-tight border rounded shadow appearance-none text-slate-700 focus:outline-none focus:shadow-outline"
-                            id="email"
-                            name="email"
-                            type="email"
-                            required
-                        />
-                    </div>
-                    <div className="mb-6 form-control">
-                        <label
-                            className="block mb-2 text-sm font-bold text-slate-700"
-                            htmlFor="password"
-                        >
-                            Password
-                        </label>
-                        <input
-                            className="w-full px-3 py-2 mb-3 leading-tight border rounded shadow appearance-none border-slate-500 text-slate-700 focus:outline-none focus:shadow-outline "
-                            id="password"
-                            type="password"
-                            name="password"
-                            required
-                        />
-                    </div>
-                    <div className="space-y-4 form-control">
-                        <button
-                            className="w-full px-4 py-2 font-bold bg-teal-300 rounded hover:text-white text-slate-700 hover:bg-teal-700 focus:outline-none focus:shadow-outline"
-                            // onClick={createUser}
-                        >
-                            Sign up
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-center mt-4">
-                        <p>Already have an account? please</p>
-                        <NavLink to="/login" className="ml-1 text-sky-600">
-                            login
-                        </NavLink>
-                    </div>
-                </div>
-            </form>
+            <Formik
+                onSubmit={createUser}
+                validationSchema={signUpValidationSchema}
+                initialValues={{ name: "", email: "", password: "" }}
+            >
+                {({ isSubmitting }) => (
+                    <Form className="sm:w-1/3" onSubmit={createUser}>
+                        <div className="p-4 px-8 pt-6 pb-8 mb-4 bg-white border rounded-md shadow-md border-slate-500">
+                            {(error || message) && (
+                                <p className="p-4 mb-4 bg-red-200 rounded-md">
+                                    {error?.message || message}
+                                </p>
+                            )}
+                            <div className="mb-4 form-control">
+                                <label
+                                    className="block mb-2 text-sm font-bold text-slate-700"
+                                    htmlFor="name"
+                                >
+                                    Name
+                                </label>
+                                <Field
+                                    className="w-full px-3 py-2 leading-tight border rounded shadow appearance-none text-slate-700 focus:outline-none focus:shadow-outline"
+                                    id="name"
+                                    type="text"
+                                    name="name"
+                                    required
+                                />
+                                <ErrorMessage
+                                    className="text-error"
+                                    name="name"
+                                    component="div"
+                                />
+                            </div>
+                            <div className="mb-4 form-control">
+                                <label
+                                    className="block mb-2 text-sm font-bold text-slate-700"
+                                    htmlFor="email"
+                                >
+                                    Email
+                                </label>
+                                <Field
+                                    className="w-full px-3 py-2 leading-tight border rounded shadow appearance-none text-slate-700 focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                />
+                                <ErrorMessage
+                                    className="text-error"
+                                    name="email"
+                                    component="div"
+                                />
+                            </div>
+                            <div className="mb-6 form-control">
+                                <label
+                                    className="block mb-2 text-sm font-bold text-slate-700"
+                                    htmlFor="password"
+                                >
+                                    Password
+                                </label>
+                                <Field
+                                    className="w-full px-3 py-2 mb-3 leading-tight border rounded shadow appearance-none border-slate-500 text-slate-700 focus:outline-none focus:shadow-outline "
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    required
+                                />
+                                <ErrorMessage
+                                    className="text-error"
+                                    name="password"
+                                    component="div"
+                                />
+                            </div>
+                            <div className="space-y-4 form-control">
+                                <button
+                                    className="w-full px-4 py-2 font-bold bg-teal-300 rounded hover:text-white text-slate-700 hover:bg-teal-700 focus:outline-none focus:shadow-outline"
+                                    disabled={isSubmitting}
+                                >
+                                    Sign up
+                                </button>
+                            </div>
+                            <div className="flex items-center justify-center mt-4">
+                                <p>Already have an account? please</p>
+                                <NavLink
+                                    to="/login"
+                                    className="ml-1 text-sky-600"
+                                >
+                                    login
+                                </NavLink>
+                            </div>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };
