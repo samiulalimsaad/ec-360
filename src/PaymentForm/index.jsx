e6a313af6ece9faf7f85f2d40ce41b6f55cf30d9
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-24 00:59:00 +0600

    payment added

---
+++ src/PaymentForm/index.jsx
@@ -0,0 +1,85 @@
+import {
+    CardElement,
+    Elements,
+    useElements,
+    useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+
+const stripePromise = loadStripe(process.env.VITE_card_key);
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        // Block native form submission.
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+        }
+
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: "card",
+            card,
+        });
+
+        if (error) {
+            console.log("[error]", error);
+        } else {
+            console.log("[PaymentMethod]", paymentMethod);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: "16px",
+                            color: "#424770",
+                            "::placeholder": {
+                                color: "#aab7c4",
+                            },
+                        },
+                        invalid: {
+                            color: "#9e2146",
+                        },
+                    },
+                }}
+            />
+            <div className="mt-6 form-control">
+                <button
+                    className="btn btn-primary"
+                    type="submit"
+                    disabled={!stripe || !elements}
+                >
+                    Pay
+                </button>
+            </div>
+        </form>
+    );
+};
+
+const PaymentForm = () => (
+    <Elements stripe={stripePromise}>
+        <CheckoutForm />
+    </Elements>
+);
+
+export default PaymentForm;
