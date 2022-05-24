19c59a1928daf4b43a1f0bcc2ce5dabe9cea9be6
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-24 11:25:44 +0600

    react query with axios added

---
+++ src/utilities/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://localhost:5000",
+    headers: {
+        "Content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+});
+
+export default apiClient;
