13e727a8be187462cdfe42cad1434d2df0ddfddc
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-25 12:45:49 +0600

    nav items added foradmin only

---
+++ src/Dashboard/AdminNavItems.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const AdminNavItems = () => {
+    return (
+        <>
+            <li>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? "bg-base-300 font-semibold" : ""
+                    }
+                    to="/dashboard/orders"
+                >
+                    Manage All Orders
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? "bg-base-300 font-semibold" : ""
+                    }
+                    to="/dashboard/add-product"
+                >
+                    Add a Product
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? "bg-base-300 font-semibold" : ""
+                    }
+                    to="/dashboard/manage-products"
+                >
+                    Manage Products
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? "bg-base-300 font-semibold" : ""
+                    }
+                    to="/dashboard/make-admin"
+                >
+                    Make Admin
+                </NavLink>
+            </li>
+        </>
+    );
+};
+
+export default AdminNavItems;
