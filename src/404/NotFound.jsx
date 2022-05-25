2e8ef4f164882c5a9d047fcbefaf6324b7c77f6f
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-25 11:44:49 +0600

    404 page added

---
+++ src/404/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import useTitle from "../utilities/useTitle";
+
+const NotFound = () => {
+    useTitle("404");
+    return (
+        <div className="flex items-center justify-center h-screen font-extrabold text-info sm:text-9xl">
+            404 Not Found
+        </div>
+    );
+};
+
+export default NotFound;
