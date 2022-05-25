82855d2cad16cb81560779d391836b3e0ae98b3f
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-25 12:14:18 +0600

    dashboard protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Dashboard from "./Dashboard";
-import AddReview from "./Dashboard/AddReview";
-import Orders from "./Dashboard/Orders";
-import Profile from "./Dashboard/Profile";
 import Footer from "./Footer";
-import Home from "./Home";
-import Login from "./Login";
 import Navbar from "./Navbar";
-import Purchase from "./Purchase";
-import Signup from "./Signup";
-import PrivateRoute from "./utilities/PrivateRoute";
+import RouteNames from "./routes";
 
 function App() {
     return (
@@ -19,24 +10,7 @@ function App() {
                 <Navbar />
             </header>
             <main>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="signup" element={<Signup />} />
-                    <Route
-                        path="purchase/:id"
-                        element={
-                            <PrivateRoute>
-                                <Purchase />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route path="dashboard" element={<Dashboard />}>
-                        <Route index element={<Profile />} />
-                        <Route path="orders" element={<Orders />} />
-                        <Route path="add-review" element={<AddReview />} />
-                    </Route>
-                </Routes>
+                <RouteNames />
             </main>
             <footer>
                 <Footer />
