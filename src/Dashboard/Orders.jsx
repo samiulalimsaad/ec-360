cdbf13201d1b74978995ca8c9c1ed1f915d49bec
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-25 13:14:02 +0600

    temporary orders page added

--- src/Dashboard/Orders.jsx
+++ src/Dashboard/Orders.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useQuery } from "react-query";
+import apiClient from "../utilities/axios";
 
 const Orders = () => {
+    const { isLoading, error, data } = useQuery(
+        ["orders"],
+        async () => (await apiClient("/orders")).data
+    );
+
+    console.log(data);
+
     return <div>Orders</div>;
 };
 
